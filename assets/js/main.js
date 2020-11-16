f84cc6a3eaa492b77dd63832dc9f16a5080d7983
edoardocollo <edoardo.collo@gmail.com>
2020-11-16 17:48:48 +0100

    general review

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,39 +19,52 @@ btnGenera.addEventListener('click',
     var ticketUsername = document.getElementById('ticket_username');
     ticketUsername.innerHTML = userName;
 
+
+    // ciclo if passaggio valori
     var costoKm = 0.21;
 
     if (tariffa == 'minorenne') {
+
+
+
       var offerta = document.getElementById('offerta');
       offerta.innerHTML = tariffa;
-      var carrozza = document.getElementById('carrozza');
-      carrozza.innerHTML = Math.floor(Math.random() * (9 - 0) + 0);
-
-      var codicaCp = document.getElementById('codice_cp');
-      codicaCp.innerHTML = Math.floor(Math.random() * (100000 - 20000) + 20000);
       var costoBiglietto = document.getElementById('costo_biglietto');
       costoBiglietto.innerHTML = (kmTratta * costoKm * 0.8).toFixed(2);
+
+
+
     } else if (tariffa == 'silver') {
+
+
+
+
       var offerta = document.getElementById('offerta');
       offerta.innerHTML = tariffa;
-      var carrozza = document.getElementById('carrozza');
-      carrozza.innerHTML = Math.floor(Math.random() * (9 - 0) + 0);
-      var codicaCp = document.getElementById('codice_cp');
-      codicaCp.innerHTML = Math.floor(Math.random() * (100000 - 20000) + 20000);
       var costoBiglietto = document.getElementById('costo_biglietto');
       costoBiglietto.innerHTML = (kmTratta * costoKm * 0.6).toFixed(2);
 
+
+
+
     }else {
+
+
+
+
       var offerta = document.getElementById('offerta');
       offerta.innerHTML = tariffa;
-      var carrozza = document.getElementById('carrozza');
-      carrozza.innerHTML = Math.floor(Math.random() * (9 - 0) + 0);
-      var codicaCp = document.getElementById('codice_cp');
-      codicaCp.innerHTML = Math.floor(Math.random() * (100000 - 20000) + 20000);
       var costoBiglietto = document.getElementById('costo_biglietto');
       costoBiglietto.innerHTML = (kmTratta * costoKm).toFixed(2);
 
     }
+
+    var carrozza = document.getElementById('carrozza');
+    carrozza.innerHTML = Math.floor(Math.random() * (9 - 0) + 0);
+    var codicaCp = document.getElementById('codice_cp');
+    codicaCp.innerHTML = Math.floor(Math.random() * (100000 - 20000) + 20000);
+
+
     var ticketSection = document.getElementById('ticket_section');
     ticketSection.style.display = 'flex';
 
