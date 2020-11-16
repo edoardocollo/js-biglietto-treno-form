3b2c1a0e8e61300fa2711e7a3ef43c25444bf200
edoardocollo <edoardo.collo@gmail.com>
2020-11-17 00:22:31 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,40 @@
 var btnGenera = document.getElementById('genera');
 var btnAnnulla = document.getElementById('annulla');
 
-// raccolta imput
+// bottone genera
 btnGenera.addEventListener('click',
   function(){
     console.log ('ticket');
 
+
+    // variabili target
     var userNameInput = document.getElementById('user_name');
     var kmTrattaInput = document.getElementById('km_tratta');
     var tariffaInput = document.getElementById('fascia_eta');
-    var userName = userNameInput.value;
-    var kmTratta = kmTrattaInput.value;
-    var tariffa = tariffaInput.value;
-    console.log (userName, kmTratta, tariffa);
 
+
+
+    // ottengo valori target
+    var userName = userNameInput.value;
+    var kmTratta =  kmTrattaInput.value;
+    // validazione kmTratta
+    if (isNaN(kmTratta)){
+      var formSection = document.getElementById('form_section');
+      formSection.style.display = 'none';
+      var crossOver = document.getElementById('cross_over');
+      crossOver.style.display = 'block';
+      var refresh = document.getElementById('refresh');
+      refresh.addEventListener ('click',
+        function(){
+          window.location.reload(false);
+        }
+      );
+    }
+    var tariffa = tariffaInput.value;
+    // console.log (userName, kmTratta, tariffa);
+
+
+    // inserimento valore username
     var ticketUsername = document.getElementById('ticket_username');
     ticketUsername.innerHTML = userName;
 
@@ -59,14 +80,17 @@ btnGenera.addEventListener('click',
 
     }
 
+
+    // generazione valori casuali carrozza e cp
     var carrozza = document.getElementById('carrozza');
     carrozza.innerHTML = Math.floor(Math.random() * (9 - 0) + 0);
     var codicaCp = document.getElementById('codice_cp');
     codicaCp.innerHTML = Math.floor(Math.random() * (100000 - 20000) + 20000);
 
-
+    // visualizza biglietto
     var ticketSection = document.getElementById('ticket_section');
-    ticketSection.style.display = 'flex';
+
+    ticketSection.style.opacity = '1';
 
 
   }
@@ -75,8 +99,7 @@ btnGenera.addEventListener('click',
 
 btnAnnulla.addEventListener ('click',
   function(){
-    console.log('annulla');
     var ticketSection = document.getElementById('ticket_section');
-    ticketSection.style.display = 'none';
+    ticketSection.style.opacity = '0';
   }
 );
